535d432a7876dba21d24fb5fee5f5b1a1b0710ac
prietopedro <pedro.prieto003@gmail.com>
2023-02-02 17:47:51 -0500

    Fix production url for mongodb

--- apiserver/config/db.js
+++ apiserver/config/db.js
@@ -4,7 +4,8 @@ const connectDB = async () => {
     try {
         mongoose.set('strictQuery', false)
         // const conn = await mongoose.connect(process.env.MONGODB_URL);
-        const conn = await mongoose.connect('mongodb://root:example@mongo:27017/backend?authSource=admin');
+        const connectionString = process.env.MONGO_DB_URL || 'mongodb://root:example@mongo:27017/backend?authSource=admin'
+        const conn = await mongoose.connect(connectionString);
         console.log(`MONGODB CONNECTED: ${conn.connection.host}`)
     } catch (error) {
         console.log(error);
