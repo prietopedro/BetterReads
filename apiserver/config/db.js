a32b2458a227c6b2b72eac238a82fbf9e76620f0
prietopedro <pedro.prieto003@gmail.com>
2023-02-02 19:11:24 -0500

    Mongodb troubleshoot

--- apiserver/config/db.js
+++ apiserver/config/db.js
@@ -5,7 +5,7 @@ const connectDB = async () => {
         mongoose.set('strictQuery', false)
         // const conn = await mongoose.connect(process.env.MONGODB_URL);
         const connectionString = process.env.MONGO_DB_URL || 'mongodb://root:example@mongo:27017/backend?authSource=admin'
-        console.log(connectionString)
+        console.log("CONNECTION STRING: ", connectionString)
         const conn = await mongoose.connect(connectionString);
         console.log(`MONGODB CONNECTED: ${conn.connection.host}`)
     } catch (error) {
