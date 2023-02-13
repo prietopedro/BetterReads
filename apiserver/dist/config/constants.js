91e3110fea108e1618b2afb2d05dc2942b98517c
prietopedro <pedro.prieto003@gmail.com>
2023-02-13 16:28:31 -0500

    tsconfig

--- apiserver/dist/config/constants.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.JWT_EXPIRES_IN = exports.NODE_ENV = exports.JWT_SECRET = exports.PORT = void 0;
-exports.PORT = process.env.PORT ? parseInt(process.env.PORT) : 3001;
-exports.JWT_SECRET = process.env.JWT_SECRET || "this is max secret, you better hope there is a jwt secret in environment variables";
-exports.NODE_ENV = process.env.NODE_ENV || "development";
-exports.JWT_EXPIRES_IN = process.env.JWT_EXPIRES_IN || "90d";
-//# sourceMappingURL=constants.js.map
